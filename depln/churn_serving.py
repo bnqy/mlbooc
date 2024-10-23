0f43a30ce4a2303ecb16c303f967a9beee472fcc
bnqy <obbonobo@gmail.com>
2024-10-23 22:52:16 +0600

    web

--- depln/churn_serving.py
+++ depln/churn_serving.py
@@ -1,6 +1,8 @@
 import pickle
 import numpy as np
 
+from flask import Flask, request, jsonify
+
 def predict_single(customer, dv, model):
     X = dv.transform([customer])
     y_pred = model.predict_proba(X)[:, 1]
@@ -9,35 +11,60 @@ def predict_single(customer, dv, model):
 with open('churn-model.bin', 'rb') as f_in:
     dv, model = pickle.load(f_in)
 
-customer = {
-    'customerid': '8879-zkjof',
-    'gender': 'female',
-    'seniorcitizen': 0,
-    'partner': 'no',
-    'dependents': 'no',
-    'tenure': 41,
-    'phoneservice': 'yes',
-    'multiplelines': 'no',
-    'internetservice': 'dsl',
-    'onlinesecurity': 'yes',
-    'onlinebackup': 'no',
-    'deviceprotection': 'yes',
-    'techsupport': 'yes',
-    'streamingtv': 'yes',
-    'streamingmovies': 'yes',
-    'contract': 'one_year',
-    'paperlessbilling': 'yes',
-    'paymentmethod': 'bank_transfer_(automatic)',
-    'monthlycharges': 79.85,
-    'totalcharges': 3320.75,
-}
+# customer = {
+#     'customerid': '8879-zkjof',
+#     'gender': 'female',
+#     'seniorcitizen': 0,
+#     'partner': 'no',
+#     'dependents': 'no',
+#     'tenure': 41,
+#     'phoneservice': 'yes',
+#     'multiplelines': 'no',
+#     'internetservice': 'dsl',
+#     'onlinesecurity': 'yes',
+#     'onlinebackup': 'no',
+#     'deviceprotection': 'yes',
+#     'techsupport': 'yes',
+#     'streamingtv': 'yes',
+#     'streamingmovies': 'yes',
+#     'contract': 'one_year',
+#     'paperlessbilling': 'yes',
+#     'paymentmethod': 'bank_transfer_(automatic)',
+#     'monthlycharges': 79.85,
+#     'totalcharges': 3320.75,
+# }
     
 
-prediction = predict_single(customer, dv, model)
+# rediction = predict_single(customer, dv, model)
 
-print('prediction: %.3f' % prediction)
+# print('prediction: %.3f' % prediction)
 
-if prediction >= 0.5:
-    print('verdict: Churn')
-else:
-    print('verdict: Not churn')
+# if prediction >= 0.5:
+#     print('verdict: Churn')
+# else:
+#     print('verdict: Not churn')
+
+
+app = Flask('churn')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    customer = request.get_json()
+
+    prediction = predict_single(customer, dv, model)
+    churn = prediction >= 0.5
+    
+    result = {
+        'churn_probability': float(prediction),
+        'churn': bool(churn),
+    }
+
+    return jsonify(result)
+
+@app.route('/dualipa', methods=['GET'])
+def dualipa():
+    return 'Dua is the Lipa'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9696)
